bd56e571507af3bc11e63f884f10a39f9ea4752b
Weng Kitt <33577934+wengkitt969696@users.noreply.github.com>
2021-04-20 16:34:50 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+if (isDev) {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+  });
+}
---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App() {
+  return <h1>I'm React App</h1>;
+}
