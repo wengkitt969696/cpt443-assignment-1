5716fab76769e80e111e4e75135195eca3ecf531
Weng Kitt <wengkitt969696@gmail.com>
2021-04-27 22:17:04 +0800

    Create header-bar.js

---
+++ src/components/header-bar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export const HeaderBar = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" style={{ background: "#22223b" }}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            className={classes.title}
+            style={{ color: "#f2e9e4" }}
+          >
+            English Conjunctions/Adverb/Adjectives Finder
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
