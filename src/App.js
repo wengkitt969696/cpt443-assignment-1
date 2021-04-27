005cb9e231a5181ed3f293fa02c5744f6849fa5e
Weng Kitt <wengkitt969696@gmail.com>
2021-04-27 22:17:17 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { useEffect } from "react";
 import { conjunctions, adjectives, adverbs } from "./dictionary";
 import { startState } from "./state/start-state";
 import { sampleText1 } from "./sample-text/sample-text";
+import { HeaderBar } from "./components/header-bar";
+import { LanguageBar } from "./components/language-bar";
+import { TextInputArea } from "./components/input-area";
+import { ResultArea } from "./components/result-area";
+import { ActionArea } from "./components/action-area";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 export default function App() {
   const word = [...conjunctions, ...adjectives, ...adverbs];
@@ -24,5 +31,29 @@ export default function App() {
     console.log(sampleText1.toLowerCase().split(" "));
   }, []);
 
-  return <h1>hehe</h1>;
+  return (
+    <div style={{ background: "#c9ada7" }}>
+      <HeaderBar />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <TextInputArea />
+          <ResultArea />
+          <ActionArea />
+        </div>
+
+        <LanguageBar wordList={word} />
+      </div>
+    </div>
+  );
 }
