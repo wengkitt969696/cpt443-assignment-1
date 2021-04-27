1ab8ee2a92a0c123b91f74a416cb965e1016cd01
Weng Kitt <wengkitt969696@gmail.com>
2021-04-27 22:17:10 +0800

    Create language-bar.js

---
+++ src/components/language-bar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 200,
+    backgroundColor: "#4a4e69",
+    overflow: "auto",
+    maxHeight: 459,
+    marginTop: 14,
+    marginBottom: 14,
+    borderRadius: 5,
+  },
+}));
+
+export const LanguageBar = ({ wordList }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div style={{ padding: 15, background: "#22223b" }}>
+        <Typography style={{ color: "#f2e9e4" }}>Language</Typography>
+      </div>
+      <List style={{ paddingTop: 0 }}>
+        {wordList.map((data, index) => (
+          <div>
+            <ListItem key={index}>
+              <ListItemText style={{ color: "#f2e9e4" }}>{data}</ListItemText>
+            </ListItem>
+            <Divider />
+          </div>
+        ))}
+      </List>
+    </div>
+  );
+};
